eea78ccda96d856e2614c0ca716f35aa5475012f
Enioluwa Segun <eos24@drexel.edu>
2020-02-01 12:50:36 -0500

    Networks and Devices (#22)
    * Implemented methods for Network and NetworkManager
    
    * init devops for python core
    
    * cleanup
    
    * Create CONTRIBUTING.md
    
    * Update CONTRIBUTING.md
    
    * reorganization of folder structure
    
    * wip: rudimentary implementation of device logic
    
    * styling cleanup
    
    * type definitions to satisfy mypy
    
    * changed NetworkManager to have a dictionary, add api support, skeleton code added for DataCollection
    
    * refactor of device module structure
    
    - Rearranged folders within device folder
    - Added stub configuration and adapter classes for ImpinjR420 and ImpinjXArray
    - Replaced ConfigObject with general type definition for JSONType
    
    * Update .gitignore
    
    * Update supportedDevices JSON
    
    * Update supportedDevices.json
    
    * Add getSupportedDevices method to DeviceAdapterFactory
    
    * add validation logic to Impinj device configuration classes
    
    * add missing import to xArray adapter
    
    * implement easy way of converting DeviceConfiguration to JSON or a Python object
    
    * add method in DeviceAdapterFactory to get information for specific device
    
    * Fix settings for xArray
    
    * add method for validating modified device configuration
    
    * wip: xArray adapter implementation
    
    * full implementation of xArray device adapter
    
    * r420 adapter implementation
    
    Co-authored-by: Juan-Garcia96 <jlg435@drexel.edu>

---
+++ core/collection/DataCollectionManager.py
@@ -0,0 +1,50 @@
+import signal
+
+from core.network import NetworkManager, Network
+
+
+class DataCollectionManager:
+    # This class is intended to work with an instance of NetworkManager. The NetworkManager
+    # is responsible for housing multiple user-defined networks. Once a network has been selected
+    # for data collection, this class interacts with the NetworkManager to start and stop data collection
+    # of all devices from the selected network
+
+    def __init__(self, networkManager: NetworkManager):
+        self.networkManager = networkManager
+        self.activeNetwork: Network = self.networkManager.getNetworkByIndex(0)  # default network to use
+
+    def setActiveNetwork(self, networkToUse: str):
+        self.activeNetwork = self.networkManager.findNetworkByNetworkName(networkToUse)
+
+    def getActiveNetwork(self) -> Network:
+        return self.activeNetwork
+
+    # This method starts the data collection process for all devices on the selected network.
+    # Simply iterate through the Network to get to the device and sends a start signal
+    def collectData(self, networkToUse: str):
+        # find the Network by unique network name
+        chosenNetwork: Network = self.networkManager.findNetworkByNetworkName(networkToUse)
+
+        if chosenNetwork is not None:
+            self.activeNetwork = chosenNetwork  # update active network
+            # iterate the network collection and start device interaction
+            for device in chosenNetwork.getAllDevices():
+                device.startDataCollection()
+
+    # This method stops the data collection process for all devices on the selected network.
+    # Simply iterate through the Network to get to the device and sends a stop signal
+    def terminateDataCollection(self):
+        for device in self.activeNetwork.getAllDevices():
+            device.stopDataCollection()
+
+    # This method is to handle ctrl-c in a graceful manner.
+    # NOTE: Python signal handlers are always executed in the main Python thread
+    #def handler(signum, frame):
+        # Handle cleanup which is gracefully terminating data collection
+        #print("SIGINT detected. Closing application.")
+        #self.terminateDataCollection()
+
+
+# Set the signal handler
+#signal.signal(signal.CTRL_C_EVENT, handler)
+
