330c47fe0c396ac22a9e7d95d94f3b2b6c497c4c
kac486 <kac486@drexel.edu>
2020-02-26 13:06:33 -0500

    cleaned up more merge conflicts

--- src/chariot/device/adapter/TestAdapter.py
+++ src/chariot/device/adapter/TestAdapter.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 from queue import Queue
 from typing import Union
-=======
 from typing import List
->>>>>>> 0dbc4c93e5904921c26b59b78188f3bd24f8b651
 from random import random, seed
 from chariot.device.adapter.DeviceAdapter import DeviceAdapter
 from chariot.device.configuration.TestConfiguration import TestConfiguration
@@ -15,14 +12,9 @@ class TestAdapter(DeviceAdapter):
     def __init__(self, config: TestConfiguration):
         super().__init__(config)
 
-<<<<<<< HEAD
     def _beginDataCollection(self) -> None:
         self._inCollectionEpisode = True
         self._randomizer()
-=======
-    def _beginDataCollection(self) -> None:
-        self.randomizer()
->>>>>>> 0dbc4c93e5904921c26b59b78188f3bd24f8b651
 
     # For testing purpose do things to mess up these functions and cause errors
     def _connect(self) -> None:
