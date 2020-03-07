db28a9b915eb813f67900b8af5fbcf989ecc79ca
Ryan <ryan.howardanton.hassing@gmail.com>
2020-03-07 14:52:45 -0500

    Modified run scripts to ensure clean exit

--- run-gui.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd ./gui
-yarn start & 
-sleep 5
-yarn run electron
--- run/run-core.sh
+++ run/run-core.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-cd ./src
+# Kill child processes on exit
+trap 'kill $(jobs -p)' EXIT
+
+cd ../src
 python3 -m venv .
 source ./bin/activate
 python3 -m chariot.api.webserver
