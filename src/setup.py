2613496fa9d19c57780dec4d1bb9af441bff3083
Enioluwa Segun <eos24@drexel.edu>
2020-04-01 13:19:17 -0400

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     python_requires='>=3.7',
     install_requires=[
         'flask',
-        'mysql',
+        'mysql-connector',
         'pymongo',
         'requests',
         'sllurp',
