f01ced2b1943e6990c1783ed07619a988a812ec9
Ryan Hassing <ryan.howardanton.hassing@gmail.com>
2020-05-02 14:17:03 -0400

    Old name for run-core.sh was present

--- run/run-gui.sh
+++ run/run-gui.sh
@@ -1,7 +1,7 @@
 # Kill child processes on exit
 trap 'kill $(jobs -p)' EXIT
 
-bash ./run-headless.sh # only start core if not already running
+bash ./run-core.sh # only start core if not already running
 
 export CORE_IP=`docker-machine ip default` # get IP address of core
 
