b62bf4ca235e34c992cb4b8f107b1adb7e54721f
Enioluwa Segun <enioluwasegun@gmail.com>
2020-05-30 10:19:24 -0400

    fix worker not collecting from 2 devices, update test

--- src/chariot/collection/DataCollectionWorker.py
+++ src/chariot/collection/DataCollectionWorker.py
@@ -149,7 +149,7 @@ class DataCollectionWorker:
                 self._consumerThreads.append(consumer)
         else:
             self._consumerThreads.append(HandledThread(name=f'Consumer-0', target=self._consumeDataFromDevices,
-                                                       args=(self._errorQueue, 0, 1,)))
+                                                       args=(self._errorQueue, 0, numDevices,)))
         self._running = True
         
         if self._dbWriter is not None:
