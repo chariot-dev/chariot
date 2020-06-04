c2af6216c66f40ac62b7110593bcdbaaf72662b0
John Ho <ho.john2561@gmail.com>
2020-06-04 13:36:53 -0400

    Error handling for starting data collection

--- gui/src/components/runConfirmation/RunConfirmationComponent.js
+++ gui/src/components/runConfirmation/RunConfirmationComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+import ErrorModalBody from '../shared/ErrorModalBody';
 
 const dataCollectionBaseURL = 'http://localhost:5000/chariot/api/v1.0/data';
 
@@ -15,10 +18,18 @@ class RunConfirmationComponent extends Component {
         "Database ID": this.props.location.networkProps["Database ID"],
         "Database Name": this.props.location.networkProps["Database Name"],
         "Database Type": this.props.location.networkProps["Database Type"],
-        configurationSettings: {} // Contains configId and runTime
+        configurationSettings: {}, // Contains configId and runTime
+        errorIsOpen: false,
+        errorMessage: ''
     }
 
     this.handleChange = this.handleChange.bind(this);
+    this.hideErrorModal = this.hideErrorModal.bind(this);
+  }
+
+  hideErrorModal(event) {
+    this.setState({ errorIsOpen: false});
+    event.preventDefault();
   }
 
   startDataCollection = () => {
@@ -39,29 +50,41 @@ class RunConfirmationComponent extends Component {
     fetch(dataCollectionBaseURL, requestOptions)
         .then(res => res.json())
         .then(
-            // If post was successful, then the dataCollector has been created, proceed to visualizer
-            (result) => {
-              console.log(result);
-            },
-            // If post was unsuccessful, update state and display error modal
-            (error) => {
-              console.log(error.message);
+          // If post was successful, then the dataCollector has been created, proceed to visualizer
+          (result) => {
+            if (result.success === true) { // Success
+              return; // Since going to then(), return null since no need to parse message   
             }
-        )
+            else if (result.message) { // If got a message, then failed
+              return result;
+            }
+        })
         .then(
-          () => {
+          (res) => {
+            if (res) { // Set up state to display error modal upon rerender
+              this.setState({ errorMessage: res.message }, () => { // Set the error message
+                this.setState({ errorIsOpen: true }); // Then set test error modal to true
+              }); 
+            }
+            else { // Success, so make API to start data collection then send to visualizer
               fetch(dataCollectionBaseURL + '/start?configId=' + this.state.configurationSettings["configId"])
               .then(res => res.json())
               .then(
                 // On success
                 (result) => {
-                  console.log(result)
+                  this.props.history.push({
+                    pathname: '/dataCollectionEpisodeStatus',
+                    runProps: {"Network Name": this.state["Network Name"], "Devices": this.state["Devices"], "configId": this.state.configurationSettings["configId"]}
+                  });
                 },
                 // On error
                 (error) => {
-                  console.log(error.message);
+                  this.setState({ errorMessage: error.message }, () => { // Set the error message
+                    this.setState({ errorIsOpen: true }); // Then set test error modal to true
+                  }); 
                 }
               )
+            }
           }
         )
   };
@@ -77,43 +100,58 @@ class RunConfirmationComponent extends Component {
 
 
   render() {
-    return (
-        <div className="container">
-            <div>
-                <h1> Data Collection Configuration Confirmation </h1>
-                <p> You have selected the following configuration for the data collection episode: </p>
-            </div>
-            <div>
-                <p> <b>Network Name:</b> {this.state["Network Name"]}</p>
-            </div>
-            <div>
-                <p> <b>Database ID:</b> {this.state["Database ID"]} </p>
-            </div>
-            <div>
-                <p> <b>Database Name:</b> {this.state["Database Name"]} </p>
-            </div>
-            <div>
-                <p> <b>Database Type:</b> {this.state["Database Type"]} </p>
-            </div>
-
-            <form>
-                <div className="form-group">
-                Run Time: <input type="number" className="form-control" id="runTime" onChange={this.handleChange}/>
-                </div>
-                <div className="form-group">
-                Configuration Name: <input className="form-control" id="configId" onChange={this.handleChange}/>
-                </div>
-
-                <Link to="/chooseNetwork">
-                    <Button variant="primary" className="float-left footer-button">Back</Button>
-                </Link>
-                <Link to={{ pathname: "/dataCollectionEpisodeStatus", runProps:{"Network Name": this.state["Network Name"], "Devices": this.state["Devices"], "configId": this.state.configurationSettings["configId"]} }}>
-                    <Button type="submit" variant="primary" className="float-right footer-button" onClick={this.startDataCollection}>Begin Collection</Button>
-                </Link>
-            </form>
-
+    return [
+      <div className="container">
+        <div>
+            <h1> Data Collection Configuration Confirmation </h1>
+            <p> You have selected the following configuration for the data collection episode: </p>
         </div>
-    );
+        <div>
+            <p> <b>Network Name:</b> {this.state["Network Name"]}</p>
+        </div>
+        <div>
+            <p> <b>Database ID:</b> {this.state["Database ID"]} </p>
+        </div>
+        <div>
+            <p> <b>Database Name:</b> {this.state["Database Name"]} </p>
+        </div>
+        <div>
+            <p> <b>Database Type:</b> {this.state["Database Type"]} </p>
+        </div>
+
+        <form>
+            <div className="form-group">
+            Run Time: <input type="number" className="form-control" id="runTime" onChange={this.handleChange}/>
+            </div>
+            <div className="form-group">
+            Configuration Name: <input className="form-control" id="configId" onChange={this.handleChange}/>
+            </div>
+
+            <Link to="/chooseNetwork">
+                <Button variant="primary" className="float-left footer-button">Back</Button>
+            </Link>
+            {/*
+            <Link to={{ pathname: "/dataCollectionEpisodeStatus", runProps:{"Network Name": this.state["Network Name"], "Devices": this.state["Devices"], "configId": this.state.configurationSettings["configId"]} }}>
+                <Button type="submit" variant="primary" className="float-right footer-button" onClick={this.startDataCollection}>Begin Collection</Button>
+            </Link>
+            */}
+            {/*
+            <Link to={{ pathname: "/dataCollectionEpisodeStatus", runProps:{"Network Name": this.state["Network Name"], "Devices": this.state["Devices"], "configId": this.state.configurationSettings["configId"]} }}>
+            </Link>  
+            */}      
+            <Button variant="primary" className="float-right footer-button" onClick={this.startDataCollection}>Begin Collection</Button>        
+        </form>
+      </div>,
+
+      <Modal show={this.state.errorIsOpen} key="runError">
+        <ErrorModalBody errorMessage={this.state.errorMessage}>
+        </ErrorModalBody>
+
+        <Modal.Footer>
+          <Button variant="primary" className="float-right" onClick={this.hideErrorModal}>OK</Button>
+        </Modal.Footer>
+      </Modal>
+    ];
   }
   
 }
