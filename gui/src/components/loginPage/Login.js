7d08f96bd90eb4142eead94820bfe607cb51217a
Juan Garcia <jlg435@drexel.edu>
2020-06-08 09:57:33 -0400

    adding onchange to login variables

--- gui/src/components/loginPage/Login.js
+++ gui/src/components/loginPage/Login.js
@@ -46,6 +46,44 @@ class Login extends Component {
     }
   }
 
+    login = () => {
+    // Post request's body
+    var that = this;
+    var data = {
+      "username": this.state.username,
+      "password": this.state.password
+    };
+
+    console.log(data);
+
+    // Post request options
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+
+    // Execute the post request to 'postCreateNetworkBaseUrl' with 'requestOptions' using fetch
+    fetch(loginUrl, requestOptions)
+    .then(res => res.json())
+    .then(
+      // If post was successful, update state and display success modal
+      () => {
+        that.setState({ loginSuccess: true });
+        //this.props.history.push(`/welcome`);
+      },
+      // If post was unsuccessful, update state and display error modal
+      (error) => {
+        // Once error message is set, then launch the error modal
+        this.setState({
+          errorMessage: error.message
+        }, () => {
+          this.setState({ errorIsOpen: !this.state.errorIsOpen });
+        });
+      }
+    )
+  };
+
   render() {
     return (
       <div className="general-component-bg">
@@ -61,11 +99,14 @@ class Login extends Component {
                   To get started, please log in.
                 </div>
                 <div className="form-group">
-                  <input required className="form-control" id="usernameInput" name="username" placeholder="Username or Email" />
+                  <input required className="form-control" id="usernameInput" name="username"
+                         placeholder="Username"  onChange={this.handleChange}/>
                 </div>
                 <div className="form-group input-group">
-                  <input required type={this.state.passwordVisible ? "text" : "password"} className="form-control" id="passwordInput" name="password" placeholder="Password" />
-                  <img id="passwordVisibilityImg" src={this.state.passwordImg} alt="Hide/Show Password" onClick={this.changePasswordVisibility}></img>
+                  <input required type={this.state.passwordVisible ? "text" : "password"} className="form-control"
+                         id="passwordInput" name="password" placeholder="Password" onChange={this.handleChange}  />
+                  <img id="passwordVisibilityImg" src={this.state.passwordImg} alt="Hide/Show Password"
+                       onClick={this.changePasswordVisibility}></img>
                 </div>
                 <div>
                   <input type="checkbox" id="rememberMe"/> Remember Me
